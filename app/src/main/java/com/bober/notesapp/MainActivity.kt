2c0b566312ce78456f75b764622afbfbc6f8282a
Bober1337IT <bartekpietras03@gmail.com>
2026-03-27 16:47:03 +0100

    initial commit

---
+++ app/src/main/java/com/bober/notesapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.bober.notesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Surface
+import com.bober.notesapp.ui.theme.NotesAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NotesAppTheme {
+                Surface() {
+
+                }
+            }
+        }
+    }
+}
