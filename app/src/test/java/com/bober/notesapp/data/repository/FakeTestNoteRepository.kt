a149b8338fc33ef6383a903d59526e57c799a7da
Bober1337IT <bartekpietras03@gmail.com>
2026-03-29 18:45:52 +0200

    add FakeTestNoteRepository

---
+++ app/src/test/java/com/bober/notesapp/data/repository/FakeTestNoteRepository.kt
@@ -0,0 +1,28 @@
+package com.bober.notesapp.data.repository
+
+import com.bober.notesapp.domain.model.Note
+import com.bober.notesapp.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class FakeTestNoteRepository @Inject constructor(): NoteRepository {
+
+    private val notes = mutableListOf<Note>()
+
+    override fun getNotes(): Flow<List<Note>> {
+        return flow { emit(notes) }
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+        return notes.find { it.id == id }
+    }
+
+    override suspend fun insertNote(note: Note) {
+        notes.add(note)
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        notes.remove(note)
+    }
+}
